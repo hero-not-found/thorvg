c37fc6af030ede8dba17d2bfc108e8bf9f5ef018
plant <fake@plant.dev>
2025-11-07 13:52:58 -0800

    HACK: reverse byte packing
    We need to reverse the byte packing so we can utilize the esp simd color conversion scripts. This is UNTESTED for anything other than drawing simple lottie animations.

--- src/renderer/sw_engine/tvgSwRaster.cpp
+++ src/renderer/sw_engine/tvgSwRaster.cpp
@@ -139,9 +139,12 @@ static inline uint32_t _abgrJoin(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 }
 
 
-static inline uint32_t _argbJoin(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
-{
-    return (a << 24 | r << 16 | g << 8 | b);
+static inline uint32_t _argbJoin(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
+#if LOTTIE_BYTE_SWAP
+  return (r << 24 | g << 16 | b << 8 | a);
+#else
+  return (a << 24 | r << 16 | g << 8 | b);
+#endif
 }
 
 static inline bool _blending(const SwSurface* surface)
