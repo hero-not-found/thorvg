bab92052a3964717b5d0d37320bdbd20a70ccce4
plant <fake@plant.dev>
2025-12-09 16:05:37 -0800

    Fix color swap

--- src/renderer/sw_engine/tvgSwRaster.cpp
+++ src/renderer/sw_engine/tvgSwRaster.cpp
@@ -141,7 +141,7 @@ static inline uint32_t _abgrJoin(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 
 static inline uint32_t _argbJoin(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
 #if LOTTIE_BYTE_SWAP
-  return (r << 24 | g << 16 | b << 8 | a);
+  return (b << 24 | g << 16 | r << 8 | a);
 #else
   return (a << 24 | r << 16 | g << 8 | b);
 #endif
