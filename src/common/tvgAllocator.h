05f120a69b48d8678a98375b19dfad6d64f740ed
plant <fake@plant.dev>
2025-12-10 10:01:25 -0800

    Custom memory allocation
    - spiram allocation on esp32
    - stop new/delete overrides (caused build issues)

--- src/common/tvgAllocator.h
+++ src/common/tvgAllocator.h
@@ -8,7 +8,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
 
- * The above copyright notice and this permission notice shall be included in all
+ * The above copyright notice and this permission notice shall be included in
+ all
  * copies or substantial portions of the Software.
 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -23,35 +24,61 @@
 #ifndef _TVG_ALLOCATOR_H_
 #define _TVG_ALLOCATOR_H_
 
-#include <cstdlib>
 #include <cstddef>
+#include <cstdlib>
 
-//separate memory alloators for clean customization
-namespace tvg
-{
-    template<typename T = void>
-    static inline T* malloc(size_t size)
-    {
-        return static_cast<T*>(std::malloc(size));
-    }
+#ifdef ESP_PLATFORM
+#include "esp_heap_caps.h"
+#endif
 
-    template<typename T = void>
-    static inline T* calloc(size_t nmem, size_t size)
-    {
-        return static_cast<T*>(std::calloc(nmem, size));
-    }
-
-    template<typename T = void>
-    static inline T* realloc(T* ptr, size_t size)
-    {
-        return static_cast<T*>(std::realloc(ptr, size));
-    }
-
-    template<typename T = void>
-    static inline void free(T* ptr)
-    {
-        std::free(ptr);
-    }
+// separate memory alloators for clean customization
+namespace tvg {
+template <typename T = void> static inline T *malloc(size_t size) {
+#ifdef ESP_PLATFORM
+  // Try SPIRAM first for large allocations on ESP32
+  auto ptr = heap_caps_malloc(size, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+  if (!ptr) {
+    // Fallback to internal RAM if SPIRAM allocation fails
+    ptr = std::malloc(size);
+  }
+  return static_cast<T>(ptr);
+#else
+  return static_cast<T>(std::malloc(size));
+#endif
 }
+
+template <typename T = void> static inline T *calloc(size_t nmem, size_t size) {
+#ifdef ESP_PLATFORM
+  // Try SPIRAM first for large allocations on ESP32
+  auto ptr = heap_caps_calloc(nmem, size, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+  if (!ptr) {
+    // Fallback to internal RAM if SPIRAM allocation fails
+    ptr = std::calloc(nmem, size);
+  }
+  return static_cast<T>(ptr);
+#else
+  return static_cast<T>(std::calloc(nmem, size));
+#endif
+}
+
+template <typename T = void> static inline T *realloc(T *ptr, size_t size) {
+
+#ifdef ESP_PLATFORM
+  // For ESP32, use heap_caps_realloc if the pointer was allocated with
+  // heap_caps
+  auto new_ptr =
+      heap_caps_realloc(ptr, size, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+  if (!new_ptr && size > 0) {
+    // Fallback to standard realloc
+    new_ptr = std::realloc(ptr, size);
+  }
+  return static_cast<T>(new_ptr);
+#else
+  return static_cast<T>(std::realloc(ptr, size));
+#endif
+}
+
+template <typename T = void> static inline void free(T *ptr) { std::free(ptr); }
+} // namespace tvg
 
 #endif //_TVG_ALLOCATOR_H_
