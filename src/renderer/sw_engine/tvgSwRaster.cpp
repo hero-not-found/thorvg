6e311fd097047c1a4d9d1e8df7af37ceaf7eafa3
plant <fake@plant.dev>
2025-12-10 10:00:31 -0800

    Byte swap override

--- src/renderer/sw_engine/tvgSwRaster.cpp
+++ src/renderer/sw_engine/tvgSwRaster.cpp
@@ -132,10 +132,14 @@ static inline uint32_t _abgrJoin(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 }
 
 
-static inline uint32_t _argbJoin(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
-{
-    return (a << 24 | r << 16 | g << 8 | b);
+static inline uint32_t _argbJoin(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
+#if LOTTIE_BYTE_SWAP
+  return (b << 24 | g << 16 | r << 8 | a);
+#else
+  return (a << 24 | r << 16 | g << 8 | b);
+#endif
 }
+
 
 static inline bool _blending(const SwSurface* surface)
 {
